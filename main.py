eb5702f5abd9de91c01fda8accacb69509c97c9a
RabronixCode <letscodeit10@gmail.com>
2025-01-03 06:53:51 +0100

    Main .txt operations

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("AAA")
+with open("file1.txt", "w") as file:
+    file.write("HIII!")
+
+with open("file1.txt", "r") as file:
+    content = file.read()
+    print(content)
+
+with open("file1.txt", "a") as file:
+    file.write("\nNEW LINE!")
