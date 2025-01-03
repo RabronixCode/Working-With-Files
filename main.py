1bea97698d254d031ce8fa61fa918a048571c0e9
RabronixCode <letscodeit10@gmail.com>
2025-01-03 09:52:03 +0100

    Size, creation time, modification time of the file

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import os
+import time
+
+# READ WRITE APPEND
 with open("file1.txt", "w") as file:
     file.write("HIII!")
 
@@ -8,7 +12,18 @@ with open("file1.txt", "r") as file:
 with open("file1.txt", "a") as file:
     file.write("\nNEW LINE!")
 
-
+# BINARY FILE COPY
 with open("image.png", "rb") as image:
     with open("image_copy.png", "wb") as copy:
         copy.write(image.read())
+
+path = "C:\\Users\\User\\Desktop\\Python\\Working_With_Files\\file_scripts\\file1.txt"
+
+size = os.path.getsize(path)
+print(size)
+
+
+print(os.path.getctime(path))
+creation_time = time.ctime(os.path.getctime(path))
+modification_time = time.ctime(os.path.getmtime(path))
+print(creation_time, "AAAAAA", modification_time)
